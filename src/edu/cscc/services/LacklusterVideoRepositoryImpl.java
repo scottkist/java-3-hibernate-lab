d8d43409a8953181153d205393e20022ba04d287
scottkist <scottkist@gmail.com>
2022-07-07 15:54:53 -0400

    working on getOrders

--- src/edu/cscc/services/LacklusterVideoRepositoryImpl.java
+++ src/edu/cscc/services/LacklusterVideoRepositoryImpl.java
@@ -2,14 +2,24 @@ package edu.cscc.services;
 
 import edu.cscc.exceptions.LacklusterVideoServiceException;
 import edu.cscc.models.Order;
+import edu.cscc.models.OrderLineItem;
 
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LacklusterVideoRepositoryImpl implements LacklusterVideoRepository {
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("LacklusterVideo");
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
 
     @Override
     public List<Order> getOrders() throws LacklusterVideoServiceException {
-        return null;
+        List<Order> orders = new ArrayList<>();
+        String testQuery = "Select o from Order o";
+        Query query = entityManager.createQuery(testQuery);
+        orders = query.getResultList();
+
+        return orders;
     }
 
     @Override
