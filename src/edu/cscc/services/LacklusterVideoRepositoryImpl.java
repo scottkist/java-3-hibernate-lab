0a6bcd29b946e9d0bc557096a1d22527fe16f5d0
scottkist <scottkist@gmail.com>
2022-07-10 12:05:35 -0400

    got createOrder Working.

--- src/edu/cscc/services/LacklusterVideoRepositoryImpl.java
+++ src/edu/cscc/services/LacklusterVideoRepositoryImpl.java
@@ -5,6 +5,7 @@ import edu.cscc.models.Customer;
 import edu.cscc.models.Employee;
 import edu.cscc.models.Order;
 import edu.cscc.models.OrderLineItem;
+
 import javax.persistence.*;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -35,40 +36,43 @@ public class LacklusterVideoRepositoryImpl implements LacklusterVideoRepository 
         /////////////////////////////////////////////////////////////// This is working
         try {
             entityManager.getTransaction().begin();
-            Employee employee = entityManager.find(Employee.class,employeeId);
-            Customer customer = entityManager.find(Customer.class,customerId);
-            Order newOrder = new Order(employee,customer,Order.DEFAULT_STORE_NUMBER);
+            Employee employee = entityManager.find(Employee.class, employeeId);
+            Customer customer = entityManager.find(Customer.class, customerId);
+            Order newOrder = new Order(employee, customer, Order.DEFAULT_STORE_NUMBER);
             entityManager.persist(newOrder);
             entityManager.getTransaction().commit();
-
-            //TODO: Get Order ID - Previous lab did Order by Dec and Limited by 1. SO trying the same solution..
-//            Query queryForOrderId = entityManager.createNativeQuery("SELECT id From orders ORDER BY id DESC LIMIT 1");
-//            orderIds = queryForOrderId.getResultList();
-//            Integer test = ((Integer)orderIds.get(0)
-//            );
-//            System.out.println("testing");
-//
-//
-//                for (int i = 0; i < rentalIds.size(); i++) {
-//                    Query sqlCreateOLI = entityManager.createNativeQuery("insert into order_line_items (order_id, rental_id) values (:orderId, :rentalId)");
-////                    sqlCreateOLI.setParameter("orderId", orderIds.get(1));
-//                    sqlCreateOLI.setParameter("rentalId", rentalIds.get(i));
-//                    sqlCreateOLI.executeUpdate();
-//            }
-//            entityManager.getTransaction().commit();
-
-            } catch (Exception exception) {
+            createOrderLineItem(rentalIds);
+        } catch (Exception exception) {
             entityManager.getTransaction().rollback();
             exception.printStackTrace();
             throw new LacklusterVideoServiceException("Could not create order");
         }
     }
 
+    public Integer findLastOrderId() {
+        TypedQuery<Integer> orderTypedQuery = entityManager.createQuery("select o.id from Order o order by o.id desc", Integer.class);
+        orderTypedQuery.setMaxResults(1);
+        Integer lastOrderId = orderTypedQuery.getSingleResult();
+        System.out.println(lastOrderId);
+        return lastOrderId;
+    }
+
+    private void createOrderLineItem(List<Integer> rentalIds) {
+        entityManager.getTransaction().begin();
+        for (int i = 0; i < rentalIds.size(); i++) {
+            Query sqlCreateOLI = entityManager.createNativeQuery("insert into order_line_items (order_id, rental_id) values (:orderId, :rentalId)");
+            sqlCreateOLI.setParameter("orderId", findLastOrderId());
+            sqlCreateOLI.setParameter("rentalId", rentalIds.get(i));
+            sqlCreateOLI.executeUpdate();
+        }
+        entityManager.getTransaction().commit();
+    }
+
     @Override
     public void deleteOrders() throws LacklusterVideoServiceException {
         try {
             entityManager.getTransaction().begin();
-            Query deleteOrderLineItems= entityManager.createQuery("delete from OrderLineItem");
+            Query deleteOrderLineItems = entityManager.createQuery("delete from OrderLineItem");
             Query deleteQueryOrders = entityManager.createQuery("delete from Order");
             deleteOrderLineItems.executeUpdate();
             deleteQueryOrders.executeUpdate();
